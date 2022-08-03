c11623d150774f2b6d531d91d29a0e40e17a94ec
Suhel-Kap <suhelkapadia2@gmail.com>
2022-08-03 10:04:16 +0530

    hydration error resolved

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,15 +1,15 @@
-import { sequence } from "0xsequence";
-import { ETHAuth, Proof } from "@0xsequence/ethauth";
-import { setCookie, getCookie } from "cookies-next";
-import React from "react";
+import { sequence } from "0xsequence"
+import { ETHAuth } from "@0xsequence/ethauth"
+import { setCookie, getCookie, deleteCookie } from "cookies-next"
+import React, { useState } from "react"
+import Link from "next/link"
+import "../styles/App.module.css"
+import Bottombar from "../components/Bottombar"
 
-export default function Login(props) {
-    const network = "mumbai";
-    sequence.initWallet(network, {
-        networkRpcUrl: "https://matic-mumbai.chainstacklabs.com",
-    });
+export default function Login() {
+    const [isLoggedIn, setIsLoggedIn] = useState(getCookie("loggedIn") || false)
     const connect = async () => {
-        const wallet = sequence.getWallet();
+        const wallet = sequence.getWallet()
         const connectDetails = await wallet.connect({
             app: "Moog3",
             authorize: true,
@@ -22,30 +22,72 @@ export default function Login(props) {
                     defaultPurchaseAmount: 111,
                 },
             },
-        });
-        console.warn("connectDetails", { connectDetails });
+        })
+        console.warn("connectDetails", { connectDetails })
 
-        const ethAuth = new ETHAuth();
+        const ethAuth = new ETHAuth()
         if (connectDetails.proof) {
-            const decodedProof = await ethAuth.decodeProof(
-                connectDetails.proof.proofString,
-                true
-            );
+            const decodedProof = await ethAuth.decodeProof(connectDetails.proof.proofString, true)
             const isValid = await wallet.utils.isValidTypedDataSignature(
                 await wallet.getAddress(),
                 connectDetails.proof.typedData,
                 decodedProof.signature,
                 await wallet.getAuthChainId()
-            );
-            console.log("isValid?", isValid);
-            setCookie("wallet", await wallet.getAddress(), { path: "/" });
-            setCookie("loggedIn", isValid, { path: "/" });
-            if (!isValid) throw new Error("sig invalid");
+            )
+            console.log("isValid?", isValid)
+            setCookie("wallet", await wallet.getAddress(), { path: "/" })
+            setCookie("loggedIn", isValid, { path: "/" })
+            if (!isValid) throw new Error("sig invalid")
         }
-    };
+    }
+
+    const disconnet = async () => {
+        const wallet = sequence.getWallet()
+        wallet.disconnect()
+        deleteCookie("wallet", { path: "/" })
+        deleteCookie("loggedIn", { path: "/" })
+        setIsLoggedIn(false)
+    }
     return (
-        <button onClick={() => props.handleClick}>
-            Sign in with Sequence Wallet
-        </button>
-    );
+        <>
+            {isLoggedIn ? (
+                <div className="page">
+                    <div className="bottomBar">
+                        <Bottombar />
+                        <div className="logout" onClick={async () => await disconnet()}>
+                            Logout
+                        </div>
+                    </div>
+                    <div className="mainWindow">
+                        <Link href="/profile">
+                            <a>Profile</a>
+                        </Link>
+                        <Link href="/settings">
+                            <a>Settings</a>
+                        </Link>
+                        <Link href="/selectedList">
+                            <a>Selected List</a>
+                        </Link>
+                        <Link href="/findYN">
+                            <a>Find Your Network</a>
+                        </Link>
+                        <Link href="/messages">
+                            <a>Messages</a>
+                        </Link>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <button
+                        onClick={async () => {
+                            await connect()
+                            setIsLoggedIn(getCookie("loggedIn") || false)
+                        }}
+                    >
+                        Login with Sequence Wallet
+                    </button>
+                </div>
+            )}
+        </>
+    )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,54 +1,9 @@
-import React, { useState } from "react"
 import Head from "next/head"
-import "../styles/App.module.css"
-import Bottombar from "../components/Bottombar"
-import Link from "next/link"
-import { setCookie, getCookie, deleteCookie } from "cookies-next"
-import { sequence } from "0xsequence"
-import { ETHAuth } from "@0xsequence/ethauth"
+import dynamic from "next/dynamic"
+
+const Login = dynamic(() => import("../components/Login"), { ssr: false })
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(getCookie("loggedIn") || false)
-    const connect = async () => {
-        const wallet = sequence.getWallet()
-        const connectDetails = await wallet.connect({
-            app: "Moog3",
-            authorize: true,
-            // keepWalletOpened: true,
-            ...{
-                settings: {
-                    theme: "indigoDark",
-                    includedPaymentProviders: ["moonpay"],
-                    defaultFundingCurrency: "matic",
-                    defaultPurchaseAmount: 111,
-                },
-            },
-        })
-        console.warn("connectDetails", { connectDetails })
-
-        const ethAuth = new ETHAuth()
-        if (connectDetails.proof) {
-            const decodedProof = await ethAuth.decodeProof(connectDetails.proof.proofString, true)
-            const isValid = await wallet.utils.isValidTypedDataSignature(
-                await wallet.getAddress(),
-                connectDetails.proof.typedData,
-                decodedProof.signature,
-                await wallet.getAuthChainId()
-            )
-            console.log("isValid?", isValid)
-            setCookie("wallet", await wallet.getAddress(), { path: "/" })
-            setCookie("loggedIn", isValid, { path: "/" })
-            if (!isValid) throw new Error("sig invalid")
-        }
-    }
-
-    const disconnet = async () => {
-        const wallet = sequence.getWallet()
-        wallet.disconnect()
-        deleteCookie("wallet", { path: "/" })
-        deleteCookie("loggedIn", { path: "/" })
-        setIsLoggedIn(false)
-    }
     return (
         <>
             <Head>
@@ -56,44 +11,7 @@ function App() {
                 <meta name="description" content="Moog3 - web3 social network" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            {isLoggedIn ? (
-                <div className="page">
-                    <div className="bottomBar">
-                        <Bottombar />
-                        <div className="logout" onClick={async () => await disconnet()}>
-                            Logout
-                        </div>
-                    </div>
-                    <div className="mainWindow">
-                        <Link href="/profile">
-                            <a>Profile</a>
-                        </Link>
-                        <Link href="/settings">
-                            <a>Settings</a>
-                        </Link>
-                        <Link href="/selectedList">
-                            <a>Selected List</a>
-                        </Link>
-                        <Link href="/findYN">
-                            <a>Find Your Network</a>
-                        </Link>
-                        <Link href="/messages">
-                            <a>Messages</a>
-                        </Link>
-                    </div>
-                </div>
-            ) : (
-                <div>
-                    <button
-                        onClick={async () => {
-                            await connect()
-                            setIsLoggedIn(getCookie("loggedIn") || false)
-                        }}
-                    >
-                        Login with Sequence Wallet
-                    </button>
-                </div>
-            )}
+            <Login />
         </>
     )
 }
