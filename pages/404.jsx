c84159aee71c4a99efe747a9550ca9e406ab13bf
SweetReptilian <tatilunamoon@gmail.com>
2022-08-20 13:38:13 -0400

    added posts and 404 pages

---
+++ pages/404.jsx
@@ -0,0 +1,31 @@
+import styles from "../styles/SettingStyles.module.scss"
+
+
+export default function NotFoundError() {
+    return (
+        <>
+            <div className={styles.backgroundImg}>
+                <div className={styles.errorCard}>
+                    <div className={styles.errorDiv}>
+                        <div className={styles.errorRight}>
+                            <img className={styles.errorImg}
+                                alt={"logoGIf"}
+                                src="./404.png"
+                                draggable={false} />
+                        </div>
+                        <div className={styles.errorLeft}>
+                            <div className={styles.errorTitle}><span>Oops!</span> <br />  I think we can't find this page</div>
+                            <div className={styles.errorWelcomeText}>Don't worry, this button will guide you to a safe place...</div>
+                            <button className={styles.goBack}>
+                                <a href="/">Let's go!</a>
+                            </button>
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
