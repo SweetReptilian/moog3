b839040b8df5ccc52285a240a8453459f28c8807
Suhel-Kap <suhelkapadia2@gmail.com>
2022-08-23 21:00:05 +0530

    spheron done

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express')
 const next = require('next')
 const { createProxyMiddleware } = require("http-proxy-middleware")
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 const apiPaths = {
     '/api': {
-        target: 'https://moog3.herokuapp.com',
+        target: 'https://moog3.herokuapp.com/',
         pathRewrite: {
             '^/api': '/api'
         },
